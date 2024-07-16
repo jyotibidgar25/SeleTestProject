4002b7ffbc2eff9f2e735afcc31bd0497582b097
jyotibidgar25 <jyotibidgar06@gmail.com>
2024-07-16 18:36:27 +0530

    class2 file updated Successfully...

--- src/TestSample/Class2.java
+++ src/TestSample/Class2.java
@@ -2,7 +2,16 @@ package TestSample;
 
 public class Class2 {
 
+	public void tc1() {
+		System.out.println("Test Case - 1");
+	}
 	public void tc2() {
 		System.out.println("Test Case - 2");
+	}
+	public void tc3() {
+		System.out.println("Test Case - 3");
+	}
+	public void tc4() {
+		System.out.println("Test Case - 4");
 	}
 }
