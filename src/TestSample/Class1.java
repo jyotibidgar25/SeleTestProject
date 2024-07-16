a75331bb738196bdb427417a7627559d0175cf07
jyotibidgar25 <jyotibidgar06@gmail.com>
2024-07-16 17:00:21 +0530

    Test On 16July2024

--- src/TestSample/Class1.java
+++ src/TestSample/Class1.java
@@ -1,7 +1,13 @@
-package TestSample;
+package src.TestSample;
+import org.testng.annotations.Test;
 
 public class Class1 {
+	@Test	
    public void TSa() {
 	   System.out.println("Hello World");
    }
+	@Test
+	public void login() {
+		
+	}
 }
