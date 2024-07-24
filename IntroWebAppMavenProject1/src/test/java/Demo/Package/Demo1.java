d53078cfd93f0966d816853242dc2a7701d1b587
jyotibidgar25 <jyotibidgar06@gmail.com>
2024-07-24 17:36:34 +0530

    Changes done by Today 24Jul2024

--- IntroWebAppMavenProject1/src/test/java/Demo/Package/Demo1.java
+++ IntroWebAppMavenProject1/src/test/java/Demo/Package/Demo1.java
@@ -1,10 +1,18 @@
 package Demo.Package;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello World....!!!!");
+       System.setProperty("webdriver.chrome.driver","C:\\Users\\JyotiBidgar\\OneDrive - Dynamisch\\Documents\\Selenuimweb Driver\\chromedriver-win64\\chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+       driver.navigate().to("https://rahulshettyacademy.com/");
+       driver.manage().window().maximize();
+      System.out.println(driver.getTitle());
 	}
 
 }
